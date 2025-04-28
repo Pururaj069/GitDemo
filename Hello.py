5b78803f9f36b2bb9d08f03f28b2230f5334d4a6
Pururaj069 <PururajChoudhary69@gmail.com>
2025-04-28 16:33:22 +0530

    Added divide feature

--- Hello.py
+++ Hello.py
@@ -12,3 +12,10 @@ print("Adding two numbers")
 a=int(input("Enter first number: "))
 b=int(input("Enter second number: "))
 print(a+b)
+
+## Freature 3
+print("Feature 1")
+print("Dvivde two numbers")
+a=int(input("Enter first number: "))
+b=int(input("Enter second number: "))
+print(a/b)
