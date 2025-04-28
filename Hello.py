9f00fdff5de6a126a0ecdc37a3e521a2cd391c9a
Manasvi Sharma <114683045+manasvi2821@users.noreply.github.com>
2025-04-28 16:28:58 +0530

    Feature 2

--- Hello.py
+++ Hello.py
@@ -1,5 +1,11 @@
 print("Hello World!!")
 
+# Feature 2
+print("Feature 2")
+a=int(input("Enter first number"))
+b=int(input("Enter second number"))
+print("Subtraction of numbers:-", a-b)
+
 ##Feature 1
 print("Feature 1")
 print("Adding two numbers")
